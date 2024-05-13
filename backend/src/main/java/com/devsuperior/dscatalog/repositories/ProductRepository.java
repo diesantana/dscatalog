b67ef16b352e6f3ec33e034f47af613a67a39467
Diego Santana <3c.santana@gmail.com>
2024-05-13 07:52:05 -0300

    fix: resolve o n+1 consultas

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -1,7 +1,20 @@
 package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj.id = :id")
+    Optional<Product> searchProductWithCategoriesById(Long id);
+
+    @Query(value = "SELECT obj FROM Product obj JOIN FETCH obj.categories", 
+            countQuery = "SELECT COUNT(obj) FROM Product obj JOIN obj.categories")
+    Page<Product> searchAll(Pageable pageable);
+    
 }
