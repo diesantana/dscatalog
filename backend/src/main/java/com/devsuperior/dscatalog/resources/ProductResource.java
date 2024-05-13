56c8eb224c8e70790c8ad171336091d01295f96b
Diego Santana <3c.santana@gmail.com>
2024-05-13 07:34:15 -0300

    feat: Cria o ProductResource com os métodos

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -0,0 +1,65 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.services.CategoryService;
+import com.devsuperior.dscatalog.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/products")
+public class ProductResource {
+    
+    @Autowired
+    private ProductService productService;
+    
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction)
+     {
+        Sort.Direction directionObj = Sort.Direction.fromString(direction);
+        Sort sort = Sort.by(directionObj, orderBy);
+        Pageable pageable = PageRequest.of(page, linesPerPage, sort);
+        Page<ProductDTO> result = productService.findAll(pageable);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO result = productService.findById(id);
+        return ResponseEntity.ok().body(result);
+    }
+    
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        ProductDTO result = productService.insert(dto);
+        // pegando o path do obj criado
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(result.getId()).toUri();
+        return ResponseEntity.created(uri).body(result);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> update(@RequestBody ProductDTO dto, @PathVariable Long id) {
+        ProductDTO result = productService.update(dto, id);
+        return ResponseEntity.ok().body(result);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
