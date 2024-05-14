364a64b8aeb08312fd9d6a922c19df3c52262247
Diego Santana <3c.santana@gmail.com>
2024-05-14 17:41:29 -0300

    test: Adiciona o primeiro teste do ProductRepository

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscatalog.repositories;
+
+import com.devsuperior.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+    
+    @Autowired
+    private ProductRepository repository;
+    
+    // deletar  se o id existir
+    @Test
+    public void deleteShouldDeleteObjWhenIdExists() {
+        long existingId = 1L;
+        repository.deleteById(existingId);
+        Optional<Product> result = repository.findById(existingId);
+//        Assertions.assertFalse(result.isPresent());
+        Assertions.assertTrue(result.isEmpty());
+    }
+    
+}
