28f9754b17221c9034e8867392d4ddf1fa42f511
Diego Santana <3c.santana@gmail.com>
2024-05-15 11:49:16 -0300

    test: testa as operações save e findById do ProductRepository

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,8 +1,8 @@
 package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +14,14 @@ import java.util.Optional;
 public class ProductRepositoryTests {
 
     private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
 
     @BeforeEach
     void setUp() {
         existingId = 1L;
+        countTotalProducts = 25;
+        nonExistingId = 100L;
     }
 
     @Autowired
@@ -29,6 +33,33 @@ public class ProductRepositoryTests {
         repository.deleteById(existingId);
         Optional<Product> result = repository.findById(existingId);
 //        Assertions.assertFalse(result.isPresent());
+        Assertions.assertTrue(result.isEmpty());
+    }
+    
+    @Test
+    public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+        Product product = Factory.createProduct();
+        product.setId(null);
+        
+        product = repository.save(product);
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1L, product.getId());
+    }
+    
+
+//    findById deveria retornar um Optional<Product> não vazio quando o id existir
+    @Test
+    public void findByIdShouldReturnNonEmptyOptionalWhenIdExists() {
+        
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertTrue(result.isPresent());
+    }
+    
+//    findById deveria retornar um Optional<Product> vazio quando o id não existir
+    @Test
+    public void findByIdShouldReturnEmptyOptionalWhenIdDoesNotExist() {
+    
+        Optional<Product> result = repository.findById(nonExistingId);
         Assertions.assertTrue(result.isEmpty());
     }
     
