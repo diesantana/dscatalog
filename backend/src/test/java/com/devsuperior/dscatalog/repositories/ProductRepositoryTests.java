d7511bcf3fa1bb582958917c24329ee296559ca6
Diego Santana <3c.santana@gmail.com>
2024-05-15 11:07:56 -0300

    test: Adiciona fixtures do JUnit

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,14 +12,20 @@ import java.util.Optional;
 
 @DataJpaTest
 public class ProductRepositoryTests {
-    
+
+    private long existingId;
+
+    @BeforeEach
+    void setUp() {
+        existingId = 1L;
+    }
+
     @Autowired
     private ProductRepository repository;
     
     // deletar  se o id existir
     @Test
     public void deleteShouldDeleteObjWhenIdExists() {
-        long existingId = 1L;
         repository.deleteById(existingId);
         Optional<Product> result = repository.findById(existingId);
 //        Assertions.assertFalse(result.isPresent());
